bd07ebf564c18dc82769c763cee2e4f9020127f1
Aapo Kiiso <aapo@kii.so>
2023-03-06 21:47:37 +0200

    Fix hydration mismatch in timeline group titles
    Previous TZ -> UTC -> TZ conversion has been removed.

--- src/components/TimelineItemGroup.tsx
+++ src/components/TimelineItemGroup.tsx
@@ -2,12 +2,14 @@ import { sortByTimeDescending } from '@/utils/sort-media-items'
 import { MouseEventHandler, MutableRefObject, useRef } from 'react'
 import { CachedGooglePhotosMediaItem } from '../lib/media-cache'
 import { getLocationLabel } from '../lib/media-item-enrichment'
-import { getDateTitle } from '../utils/date'
-import { scrollIntoView } from '../utils/scroll-into-view'
+import { formatInDisplayTimeZone } from '../utils/date'
 import TimelineItem from './TimelineItem'
 
-export default function TimelineItemGroup({ date, items, activeItemId, activeItemRef, setActiveItemId, itemVisibilityObserver, setActiveMediaItemIdWithScrollTo }: { date: string, items: CachedGooglePhotosMediaItem[], activeItemId?: string, activeItemRef?: MutableRefObject<HTMLElement|null>, setActiveItemId: (activeItemId: string|undefined) => void, itemVisibilityObserver?: IntersectionObserver, setActiveMediaItemIdWithScrollTo: (activeItemId: string|undefined) => void }) {
+export default function TimelineItemGroup({ items, activeItemId, activeItemRef, setActiveItemId, itemVisibilityObserver, setActiveMediaItemIdWithScrollTo }: { items: CachedGooglePhotosMediaItem[], activeItemId?: string, activeItemRef?: MutableRefObject<HTMLElement|null>, setActiveItemId: (activeItemId: string|undefined) => void, itemVisibilityObserver?: IntersectionObserver, setActiveMediaItemIdWithScrollTo: (activeItemId: string|undefined) => void }) {
   const sortedItems = sortByTimeDescending(items)
+
+  // Use any item's creation time as the date source, as they are already grouped to the same date.
+  const date = sortedItems.length ? sortedItems[0].mediaMetadata.creationTime : null
 
   const startLocationLabel = sortedItems.length ? getLocationLabel(sortedItems[0]) : null
   const endLocationLabel = sortedItems.length ? getLocationLabel(sortedItems[sortedItems.length - 1]) : null
@@ -28,7 +30,7 @@ export default function TimelineItemGroup({ date, items, activeItemId, activeIte
     <div className="mt-8 mb-4" ref={groupContainer}>
       <header className="inline-flex items-center sticky top-4 py-2 px-4 z-10 bg-accent-primary-500 rounded-2xl cursor-pointer" onClick={handleHeaderClick}>
         <div className="md:flex">
-          <h2 className="text-white">{getDateTitle(date)}</h2>
+          {date && <h2 className="text-white">{formatInDisplayTimeZone(date, 'E, MMM d')}</h2>}
           {locationLabel && <h3 className="text-accent-primary-200 md:ml-4">{locationLabel}</h3>}
         </div>
       </header>
